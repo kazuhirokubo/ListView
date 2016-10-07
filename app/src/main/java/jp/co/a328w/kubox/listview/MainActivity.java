0853575d5810c7a8c36c6a76fc8d6a466ff5c9a8
kubox <kubox@328w.co.jp>
2016-10-07 11:45:48 +0900

    独自モデルクラス追加

--- app/src/main/java/jp/co/a328w/kubox/listview/MainActivity.java
+++ app/src/main/java/jp/co/a328w/kubox/listview/MainActivity.java
@@ -3,11 +3,15 @@ package jp.co.a328w.kubox.listview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
